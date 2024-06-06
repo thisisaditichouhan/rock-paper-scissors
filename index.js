5e772ef2e1caa1dcb4b1c5932b96c0872b09c34f
ADITI CHOUHAN <48049926+thisisaditichouhan@users.noreply.github.com>
2024-06-06 23:52:37 +0530

    Added reset button and scorecard functions
    Added reset button and scorecard functions

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const resultEl = document.getElementById('result')
 const rockBtn = document.getElementById('rock')
 const paperBtn = document.getElementById('paper')
 const scissorsBtn = document.getElementById('scissors')
+const computerWinsEl = document.getElementById('computerWins')
+const playerWinsEl = document.getElementById('playerWins')
+const tiesEl = document.getElementById('ties')
+const resetBtn = document.getElementById('reset')
 
 
 
 let playerChoice = ""
 let computerChoice = ""
 let gameResult = ""
+let ties = 0;
+let playerWins = 0;
+let computerWins= 0;
 
 function computerChoose(){
     let randomChoice = Math.floor(Math.random()*3)+1;
@@ -27,26 +34,35 @@ function computerChoose(){
 }
 
 function resultCalculation(){
-    if(playerChoice=='Rock' && computerChoice=='Scissors'){
-        gameResult="Player Won"
-    }
-    else if(playerChoice=='Scissors' && computerChoice=='Rock'){
-        gameResult="Computer Won"
-    }
-    else if(playerChoice=='Scissors' && computerChoice=='Paper'){
-        gameResult="Player Won"
-    }
-    else if(playerChoice=='Paper' && computerChoice=='Scissors'){
-        gameResult="Computer Won"
-    }
-    else if(playerChoice=='Paper' && computerChoice=='Rock'){
+    if(playerChoice==='Rock' && computerChoice==='Scissors'){
         gameResult="Player won"
     }
-    else if(playerChoice=='Rock' && computerChoice=='Paper'){
+    else if(playerChoice==='Scissors' && computerChoice==='Rock'){
+        gameResult="Computer Won"
+    }
+    else if(playerChoice==='Scissors' && computerChoice==='Paper'){
+        gameResult="Player won"
+    }
+    else if(playerChoice==='Paper' && computerChoice==='Scissors'){
         gameResult="Computer won"
     }
-    else if(playerChoice==computerChoice){
+    else if(playerChoice==='Paper' && computerChoice==='Rock'){
+        gameResult="Player won"
+    }
+    else if(playerChoice==='Rock' && computerChoice==='Paper'){
+        gameResult="Computer won"
+    }
+    else if(playerChoice===computerChoice){
         gameResult="It is a tie!"
+    }
+    if(gameResult==="Computer won"){
+        computerWins++
+    }
+    else if(gameResult==="Player won"){
+        playerWins++
+    }
+    else if(gameResult==="It is a tie!"){
+        ties++
     }
 }
 
@@ -57,6 +73,9 @@ rockBtn.addEventListener('click',()=>{
     computerEl.innerText = `Computer: ${computerChoice}`
     resultCalculation()
     resultEl.innerText = `Result is: ${gameResult}`
+    computerWinsEl.innerText = `Computer: ${computerWins}`
+    playerWinsEl.innerText = `Player: ${playerWins} `
+    tiesEl.innerText=` Tie: ${ties}`
 
 
 })
@@ -68,6 +87,9 @@ scissorsBtn.addEventListener('click',()=>{
     computerEl.innerText = `Computer: ${computerChoice}`
     resultCalculation()
     resultEl.innerText = `Result is: ${gameResult}`
+    computerWinsEl.innerText = `Computer: ${computerWins}`
+    playerWinsEl.innerText = `Player: ${playerWins} `
+    tiesEl.innerText=` Tie: ${ties}`
 })
 
 paperBtn.addEventListener('click',()=>{
@@ -77,4 +99,24 @@ paperBtn.addEventListener('click',()=>{
     computerEl.innerText = `Computer: ${computerChoice}`
     resultCalculation()
     resultEl.innerText = `Result is: ${gameResult}`
+    computerWinsEl.innerText = `Computer: ${computerWins}`
+    playerWinsEl.innerText = `Player: ${playerWins} `
+    tiesEl.innerText=` Tie: ${ties}`
+})
+
+resetBtn.addEventListener('click',()=>{
+    playerChoice = ""
+    computerChoice = ""
+    gameResult = ""
+    ties = 0;
+    playerWins = 0;
+    computerWins= 0;
+    playerEl.innerText = `Player: Waiting for the player to make a choice`
+    
+    computerEl.innerText = `Computer's choice will reflect here`
+   
+    resultEl.innerText = `Result: Play first`
+    computerWinsEl.innerText = `Computer: ${computerWins}`
+    playerWinsEl.innerText = `Player: ${playerWins} `
+    tiesEl.innerText=` Tie: ${ties}`
 })
